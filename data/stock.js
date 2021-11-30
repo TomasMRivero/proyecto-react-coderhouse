fa5c6c2747ac07ea8fec66fb8c9800ef52a186d4
TomasMRivero <tmartinezrivero@alumno.unlam.edu.ar>
2021-11-30 15:20:17 -0300

    crear stock

---
+++ data/stock.js
@@ -0,0 +1,123 @@
+export const stock = [
+    {
+        id:1,
+        nombre:'Homero Arbusto',
+        precio:1500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'remera',
+        color:'verde',
+        talle:'S',
+        stock:20,
+        oferta: false
+    },
+    {
+        id:2,
+        nombre:'Homero Arbusto',
+        precio:1500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'remera',
+        color:'verde',
+        talle:'M',
+        stock:35,
+        oferta: false
+    },
+    {
+        id:3,
+        nombre:'Homero Arbusto',
+        precio:1500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'remera',
+        color:'verde',
+        talle:'L',
+        stock:42,
+        oferta: false
+    },
+    {
+        id:4,
+        nombre:'Homero Arbusto',
+        precio:1500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'remera',
+        color:'verde',
+        talle:'XL',
+        stock:12,
+        oferta: false
+    },
+    {
+        id:5,
+        nombre:'Homero Arbusto',
+        precio:1500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'remera',
+        color:'blanco',
+        talle:'XXL',
+        stock:5,
+        oferta: false
+    },
+    {
+        id:6,
+        nombre:'Akatsuki',
+        precio:3500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'buzo',
+        color:'verde',
+        talle:'S',
+        stock:10,
+        oferta: false
+    },
+    {
+        id:6,
+        nombre:'Akatsuki',
+        precio:3500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'buzo',
+        color:'negro',
+        talle:'S',
+        stock:10,
+        oferta: true
+    },
+    {
+        id:7,
+        nombre:'Akatsuki',
+        precio:3500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'buzo',
+        color:'blanco',
+        talle:'S',
+        stock:21,
+        oferta: true
+    },
+    {
+        id:8,
+        nombre:'Akatsuki',
+        precio:3500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'buzo',
+        color:'negro',
+        talle:'M',
+        stock:3,
+        oferta: true
+    },
+    {
+        id:9,
+        nombre:'Akatsuki',
+        precio:3500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'buzo',
+        color:'blanco',
+        talle:'M',
+        stock:11,
+        oferta: true
+    },
+    {
+        id:10,
+        nombre:'Akatsuki',
+        precio:3500,
+        descripcion:'Remera Homero Arbusto',
+        categoria:'buzo',
+        color:'negro',
+        talle:'L',
+        stock:4,
+        oferta: true
+    },
+]
