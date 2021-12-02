0e05224bf2a91161853995bccd66c65372df3da4
TomasMRivero <tmartinezrivero@alumno.unlam.edu.ar>
2021-12-02 11:05:32 -0300

    Crear grid para ItemList

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,15 +1,21 @@
+import { Grid } from "@mui/material"
 import { Item } from "../Item/Item"
 
 export const ItemList = ( { items = [] } ) => {
     return(
-        <>
         <div>
             <h2>Productos</h2>
             <hr />
+            <Grid container spacing={1}>
             {
-                items.map( (el) => <Item key={el.id} item={el}/> )
+                items.map( (el) =>
+                    <Grid item xs={3}>
+                        <Item key={el.id} item={el}/>
+                    </Grid>
+                )
             }
+            </Grid>
         </div>
-        </>
+        
     )
 }
