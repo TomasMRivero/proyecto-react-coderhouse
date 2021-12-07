db410ed8e6ddf6171a2716de030586df728e208a
TomasMRivero <tmartinezrivero@alumno.unlam.edu.ar>
2021-12-07 15:34:49 -0300

    crear cart reducer

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk';
+import { cartReducer } from '../reducers/cartReducer';
 import { productsReducer } from '../reducers/productReducer';
 import { uiReducer } from '../reducers/uiReducer';
 
 export const reducers = combineReducers({
     productos: productsReducer,
-    ui: uiReducer
+    ui: uiReducer,
+    cart: cartReducer
 })
 
 export const store = createStore(
